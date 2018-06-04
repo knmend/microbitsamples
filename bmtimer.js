ed93646e509610240f36a8eff98f7957a5bc9ca6
knmend <39860989+knmend@users.noreply.github.com>
2018-06-04 09:08:32 +0900

    blink number
    LED patterns were converted to one-liners

--- bmtimer.js
+++ bmtimer.js
@@ -1,40 +1,14 @@
-// Timer mode
-// -1: Initial screen
-//  0: Waiting for count
-//  1: Counting down timer
-//  2: Waiting
-let mode = -1
-// Miliseconds per second. Fast debugging is available with a small value.
-let tunit = 1000
-// Elapsed time counters
-let elapsed_sec = 0
 let elapsed_min = 0
 let last_minute = 0
-// Time for lap
+let tunit = 0
+let elapsed_sec = 0
 let next_lap = 0
-// Requested counter
 let minutes = 0
-// Temporary flag for starting animation
 let start_flag = false
-// Variables storing system time counts
-let wait_start = 0
-let start_time = 0
-
-// A button management
-// Set requested time
-input.onButtonPressed(Button.A, () => {
-    if (mode <= 0) {
-        mode = 0
-        if (minutes < 9) {
-            minutes += 1
-            music.playTone(900, 66)
-            basic.showNumber(minutes)
-        }
-    }
-})
-
-// B button management
-// Start when the time is set, stop or resume when the timer is running
+let mode = 0
+let ticker = 0
+// B button management Start when the time is set,
+// stop or resume when the timer is running
 input.onButtonPressed(Button.B, () => {
     if (mode == 0 && minutes > 0) {
         // start
@@ -49,13 +23,22 @@ input.onButtonPressed(Button.B, () => {
         music.playTone(900, 66)
     } else if (mode == 2) {
         mode = 1
-        start_time +=  game.currentTime() - wait_start
+        start_time += game.currentTime() - wait_start
         music.playTone(900, 66)
     }
 })
-
-// AB button mangement
-// Cancel timer
+// A button management Set requested time
+input.onButtonPressed(Button.A, () => {
+    if (mode <= 0) {
+        mode = 0
+        if (minutes < 9) {
+            minutes += 1
+            music.playTone(900, 66)
+            basic.showNumber(minutes)
+        }
+    }
+})
+// AB button mangement Cancel timer
 input.onButtonPressed(Button.AB, () => {
     if (mode == 1) {
         music.beginMelody(music.builtInMelody(Melodies.JumpDown), MelodyOptions.OnceInBackground)
@@ -64,59 +47,40 @@ input.onButtonPressed(Button.AB, () => {
         minutes = 0
     }
 })
-
+let wait_start = 0
+let start_time = 0
+// Timer mode -1: Initial screen 0: Waiting for count
+// 1: Counting down timer 2: Waiting
+mode = -1
+// Miliseconds per second. Fast debugging is available
+// with a small value.
+tunit = 100
 // Main loop
 basic.forever(() => {
     if (mode == -1) {
-        basic.showLeds(`
-            . # # # .
-            # . # . #
-            # . # . #
-            # # # # #
-            . # # # .
-            `)
+        basic.showLeds(`.###.\n#.#.#\n#.#.#\n#####\n.###.\n`)
         basic.pause(100)
     } else if (mode == 0) {
         basic.pause(10)
     } else if (mode == 1) {
-        if (start_flag) {
-            basic.showLeds(`
-                . # # # .
-                # . # . #
-                # . # . #
-                # # # # #
-                . # # # .
-                `)
+        if (start_flag) { // display start animation
+            basic.showLeds(`.###.\n#.#.#\n#.#.#\n#####\n.###.\n`)
             music.playTone(150, 33)
-            basic.showLeds(`
-                # # . . #
-                . # . . #
-                . # . . .
-                . # . . #
-                # # . . #
-                `)
+            basic.showLeds(`#.#.#\n#.#.#\n#####\n.###.\n.....`)
+            music.playTone(175, 33)
+            basic.showLeds(`#.#.#\n#####\n.###.\n.....\n.....`)
             music.playTone(200, 33)
-            basic.showLeds(`
-                # . . . .
-                # . . . .
-                # . . . .
-                # . . . .
-                # . . . .
-                `)
-            music.playTone(260, 33)
+            basic.showLeds(`#####\n.###.\n.....\n.....\n.....`)
+            music.playTone(225, 33)
+            basic.showLeds(`.###.\n.....\n.....\n.....\n.....`)
+            basic.clearScreen()
             start_flag = false
         }
         let tm = game.currentTime()
         elapsed_sec = (tm - start_time) / tunit
         elapsed_min = elapsed_sec / 60
-        if (elapsed_min >= minutes) {
-            basic.showLeds(`
-                . # . . #
-                . # . # .
-                . . . . #
-                # . . . .
-                . # # # #
-                `)
+        if (elapsed_min >= minutes) { // display yummy mark
+            basic.showLeds(`.#..#\n.#.#.\n....#\n#....\n.####`)
             music.beginMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.OnceInBackground)
             basic.pause(1000)
             mode = -1
@@ -124,16 +88,15 @@ basic.forever(() => {
         } else {
             if (last_minute != minutes - elapsed_min) {
                 last_minute = minutes - elapsed_min
-                basic.showLeds(`
-                    . . # . #
-                    # # # # .
-                    . . . # .
-                    . . . # .
-                    # # # . .
-                    `)
+                basic.showLeds(`..#.#\n####.\n...#.\n...#.\n###..`)
                 music.playTone(200, 66)
             }
-            basic.showNumber(minutes - elapsed_min)
+            if (ticker < 2) {
+                basic.showNumber(minutes - elapsed_min)
+            } else {
+                basic.clearScreen()
+            }
+            ticker = (ticker + 1) % 2
             if (elapsed_sec > next_lap) {
                 next_lap += tunit * 60
             }
