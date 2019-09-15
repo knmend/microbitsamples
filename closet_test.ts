afd5377b3cac5305b728c0916512dc95a6377c67
knmend <39860989+knmend@users.noreply.github.com>
2019-09-15 23:21:52 +0900

    Update closet_test.ts

--- closet_test.ts
+++ closet_test.ts
@@ -7,22 +7,11 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
 })
-function ShowAnimation() {
-    for (let k = 4; k < 9; k++) {
-        LEDIcon(k)
-        basic.pause(500)
-    }
-    basic.pause(1000)
-}
-function LEDIcon(index: number) {
-
-    codes = [[[512, 8537128, 271074434]], [[35, 3687554, 2189575224]], [[4, 61680, 4026531840], [801, 3598, 235407360]], [[4, 240, 4042260480], [801, 526350, 235798528]], [[562, 16, 0]], [[562, 4136, 268435456]], [[562, 3687492, 1144520704]], [[562, 1011006849, 2913288764]], [[562, 3950080, 572793344]]]
-    codes[1] = [[0x400, 0x00824428, 0x10284482]]
-    codes[0] = [[0x024, 0x00384482, 0x82824438]]
+function LEDIcon (index: number) {
     for (let i = 0; i <= 64 - 1; i++) {
         let slot = i < 32 ? 2 : 1
-        let mask = 1 << (i & 0x1f)
-        red = 0
+let mask = 1 << (i & 0x1f)
+red = 0
         green = 0
         blue = 0
         for (let j = 0; j <= codes[index].length - 1; j++) {
@@ -37,38 +26,42 @@ function LEDIcon(index: number) {
         green = Math.min(255, green)
         blue = Math.min(255, blue << 4)
         let pos = (i & 8) == 0 ? i : (i | 7) - (i & 7)
-        strip.setPixelColor(pos, neopixel.rgb(red, green, blue))
+strip.setPixelColor(pos, neopixel.rgb(red, green, blue))
     }
     strip.show()
 }
-let green = 0
-let strip: neopixel.Strip = null
-let red = 0
-let blue = 0
-let codes: number[][][] = []
+let pinsignal = 0
 let colorcode = 0
-let state = 1
+let codes: number[][][] = []
+let blue = 0
+let green = 0
+let red = 0
+let strip: neopixel.Strip = null
+let inputPins = []
 let currentPins = [1, 1, 1]
 strip = neopixel.create(DigitalPin.P1, 64, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.setBrightness(128)
+red = 0
+green = 0
+blue = 0
+codes.push([[36, 3687554, 2189575224]])
+codes.push([[1024, 8537128, 271074434]])
+codes.push([[4, 61680, 4026531840], [801, 3598, 235407360]])
+codes.push([[4, 240, 4042260480], [801, 526350, 235798528]])
 basic.forever(function () {
     if (input.lightLevel() > 0) {
-        let inputPins: number[] = []
-        let flag = 0
         for (let l = 0; l <= 2; l++) {
-            let p = 0
             if (l == 0) {
-                p = pins.digitalReadPin(DigitalPin.P8)
+                pinsignal = pins.digitalReadPin(DigitalPin.P8)
             } else if (l == 1) {
-                p = pins.digitalReadPin(DigitalPin.P2)
+                pinsignal = pins.digitalReadPin(DigitalPin.P2)
             } else if (l == 2) {
-                p = pins.digitalReadPin(DigitalPin.P16)
+                pinsignal = pins.digitalReadPin(DigitalPin.P16)
             }
-            flag |= pins.digitalReadPin(inputPins[l]) << l
-            if (currentPins[l] != p) {
-                currentPins[l] = p
-                if (p == 1) {
+            if (currentPins[l] != pinsignal) {
+                currentPins[l] = pinsignal
+                if (pinsignal == 1) {
                     if (Math.randomRange(0, 1) == 0) {
                         LEDIcon(Math.randomRange(0, 1) * 2)
                         music.beginMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
@@ -81,12 +74,6 @@ basic.forever(function () {
                 }
             }
         }
-        // if (pins.digitalReadPin(DigitalPin.P2) == 1) {
-        //     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-        //     music.playTone(330, music.beat(BeatFraction.Whole))
-        //     LEDIcon(Math.randomRange(0, 3))
-        //     basic.pause(100)
-        // }
     } else {
         control.waitMicros(1000)
     }
