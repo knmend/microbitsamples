ac5e1d304e7a9aef38788f87f630cb7dd4a108bf
knmend <39860989+knmend@users.noreply.github.com>
2020-08-10 01:24:34 +0900

    Update kaleidobbit.ts

--- kaleidobbit.ts
+++ kaleidobbit.ts
@@ -1,109 +1,189 @@
+let dy = 0
+let dx = 0
+let norm = 0
+let ay_ = 0
+let ax_ = 0
+let pixel: number[] = []
+let y = 0
+let x = 0
 let strip: neopixel.Strip = null
-let red:number = 0
-let green:number = 0
-let blue:number = 0
-let colorcode:number = 0
-let codes: number[][][] = []
+let d = 0
+let scale = 0
+let num_stars = 0
+let canvas_height = 0
+let canvas_width = 0
+let stars:number[][] = []
+let weights:number[][] = []
 let matrix:number[][][] = []
-let width:number = 0
-let height:number = 0
-let size:number = 0
-let weights:number[] = []
+let pixels_width = 8
+let pixels_height = 8
+canvas_width = 800
+canvas_height = 800
+num_stars = 10
+scale = 100
+let weight_step = 8
 
-width = 8
-height = 8
-for (let i = 0; i < 8; i++) {
-    let row:number[][] = []
-    for (let j = 0; j < 8; j++) {
-        row.push([0,0,0])
-    }
-    matrix.push(row)
-}
-for (let i = 0; i < 8; i++) {
-    weights.push(Math.exp(-(i * i * 0.3)))
-}
-
-function clearMatrix() {
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            for (let k = 0; k < 3; k++) {
-                matrix[i][j][k] = 0
-            }
+function initializeStars() {
+    for (let index = 0; index < stars.length; index++) {
+        let x = stars[index][0]
+        let y = stars[index][1]
+        if (x < 0 || x >= canvas_width || y < 0 || y >= canvas_height) {
+            x = Math.floor(Math.random() * (canvas_width - 1))
+            y = Math.floor(Math.random() * (canvas_height - 1))
+            stars[index] = [x, y, 128, 192, 255]
         }
     }
 }
 
-function displayMatrix() {
-    for (let i = 0; i < 8; i++) {
-        for (let j = 0; j < 8; j++) {
-            let pos:number = (j & 2) == 0 ? i + j * 8: (7 - i) + j * 8
-            let pixel:number[] = matrix[j][i]
+// function initializeStars (num: number, scale: number) {
+//     let points: number[][] = []
+//     for (let index = 0; index < num_stars; index++) {
+//         if (Math.random() < 0.5) {
+//             x_2 = Math.random() * scale
+//             y_2 = Math.random() < 0.5 ? 0 : scale - 1
+//         } else {
+//             y_2 = Math.random() * scale
+//             x_2 = Math.random() < 0.5 ? 0 : scale - 1
+//         }
+//         points.push([x_2, y_2])
+//     }
+//     return points
+// }
+function showStars() {
+    clearMatrix()
+    for (let i = 0; i < stars.length; i++) {
+        let star = stars[i]
+        plotStar(matrix, star[0], star[1], [star[2], star[3], star[4]])
+    }
+    displayMatrix()
+}
+input.onButtonPressed(Button.A, function () {
+    showStars()
+    // clearMatrix()
+    // for (let index = 0; index < 10; index++) {
+    //     x = Math.floor(Math.random() * canvas_width)
+    //     y = Math.floor(Math.random() * canvas_height)
+    //     plotStar(matrix, x, y, [128,128,255])
+    // }
+    // displayMatrix()
+})
+input.onButtonPressed(Button.B, function () {
+    moveStars(0.5)
+    clearMatrix()
+    for (let i = 0; i < stars.length; i++) {
+        let star = stars[i]
+        plotStar(matrix, star[0], star[1], [star[2], star[3], star[4]])
+    }
+    displayMatrix()
+})
+function displayMatrix () {
+    for (let p = 0; p <= 7; p++) {
+        for (let q = 0; q <= 7; q++) {
+            let pos:number = (q & 2) == 0 ? p + q * 8: (7 - p) + q * 8
+            pixel = matrix[q][p]
             strip.setPixelColor(pos, neopixel.rgb(pixel[0], pixel[1], pixel[2]))
         }
     }
-}
-
-function distributeStar(edge:number) {
-    
-}
-
-function plotStar(x:number, y:number, scale:number, color:number[]) {
-    let x0:number = 0
-    let y0:number = 0
-    let x1:number = 0
-    let y1:number = 0
-    x0 = x / scale - 1
-    y0 = y / scale - 1
-    x1 = x0 + 2
-    y1 = y0 + 2
-    x0 = Math.max(0, Math.round(x0))
-    y0 = Math.max(0, Math.round(y0))
-    x1 = Math.min(7, Math.round(x1))
-    y1 = Math.min(7, Math.round(y1))
-    for (let x_ = x0; x_ <= x1; x_++) {
-        let dx_ = Math.pow(x_ - x / scale, 2)
-        for (let y_ = y0; y_ < y1; y_++) {
-            let dy_ = Math.pow(y_ - y / scale, 2)
-            let dist2_ = dx_ + dy_
-            if (dist2_ >= 1) {
-                continue
-            }
-            let index = Math.floor(dist2_ * weights.length)
-            for (let i = 0; i < 3; i++) {
-                matrix[y_][x_][i] = Math.min(255, matrix[y_][x_][i] + color[i] * weights[index])
-            }
-        }
-    }   
-}
-function LEDIcon(index: number) {
-    for (let i = 0; i <= 64 - 1; i++) {
-        let slot = i < 32 ? 2 : 1
-        let mask = 1 << (i & 0x1f)
-        red = 0
-        green = 0
-        blue = 0
-        for (let j = 0; j <= codes[index].length - 1; j++) {
-            colorcode = codes[index][j][0]
-            if ((codes[index][j][slot] & mask) != 0) {
-                red += colorcode & 0xf00
-                green += colorcode & 0x0f0
-                blue += colorcode & 0xf
-            }
-        }
-        red = Math.min(255, red >> 4)
-        green = Math.min(255, green)
-        blue = Math.min(255, blue << 4)
-        let pos = (i & 8) == 0 ? i : (i | 7) - (i & 7)
-        strip.setPixelColor(pos, neopixel.rgb(red, green, blue))
-    }
     strip.show()
 }
+function clearMatrix () {
+    for (let m = 0; m <= 7; m++) {
+        for (let n = 0; n <= 7; n++) {
+            for (let o = 0; o <= 2; o++) {
+                matrix[m][n][o] = 0
+            }
+        }
+    }
+}
+function moveStars (dt: number) {
+    ax_ = input.acceleration(Dimension.X)
+    ay_ = input.acceleration(Dimension.Y)
+    if (Math.abs(ax_) < 0.001) {
+        ax_ = 0.001
+    }
+    norm = Math.sqrt(ax_ * ax_ + ay_ * ay_)
+    dx = ay_ / norm * dt * scale
+    dy = ax_ / norm * dt * scale
+    for (let i = 0; i < stars.length; i++) {
+        let x_ = stars[i][0]
+        let y_ = stars[i][1]
+        x_ += dx
+        y_ += dy
+        if (x_ < 0 || x >= canvas_width || y_ < 0 || y_ >= canvas_height) {
+            if (Math.random() < 0.5) {
+                x_ = Math.floor(Math.random() * canvas_width - 1)
+                y_ = Math.random() < 0.5 ? 0 : canvas_height - 1
+            } else {
+                y_ = Math.floor(Math.random() * canvas_height - 1)
+                x_ = Math.random() < 0.5 ? 0 : canvas_width - 1
+            }
+        }
+        stars[i][0] = x_
+        stars[i][1] = y_
+    }
+}
 
+
+function initialize() {
+    // clear matrix
+    for (let index = 0; index < pixels_height; index++) {
+        let row: number[][] = []
+        for (let index = 0; index < pixels_width; index++) {
+            row.push([0, 0, 0])
+        }
+        matrix.push(row)
+    }
+    // set coefficient constants
+    for (let k = 0; k < weight_step; k++) {
+        let row_:number[] = []
+        for (let l = 0; l < weight_step; l++) {
+            d = k * k + l * l
+            row_.push(Math.exp(- (d * d / 200)))
+        }
+        weights.push(row_)
+    }
+    // set random stars
+    stars = []
+    for (let index = 0; index < num_stars; index++) {
+        stars.push([-1,-1, 0xffffff])
+    }
+}
+
+function plotStar(matrix:number[][][], x:number, y:number, color:number[]) {
+    let x_:number = x / scale
+    let y_:number = y / scale
+    for (let r = 0; r < 2; r++) {
+        let my = Math.floor(y / scale + r)
+        if (my < 0 || my >= 8) {
+            continue
+        }
+        let wy = Math.abs(y_ - my) * weight_step
+        if (wy >= weight_step) {
+            continue
+        }
+        for (let s = 0; s < 2; s++) {
+            let mx = Math.floor(x / scale + s)
+            if (mx < 0 || mx >= 8) {
+                continue
+            }
+            let wx = Math.abs(x_ - mx) * weight_step
+            if (wx >= weight_step) {
+                continue
+            }
+            let w = weights[Math.floor(wy)][Math.floor(wx)]
+            for (let t = 0; t < 3; t++) {
+                // let c = matrix[my][mx][k]
+                // c += Math.round(color[k] * w)
+                matrix[my][mx][t] = Math.min(255, matrix[my][mx][t] + Math.round(color[t] * w))
+            }
+        }
+    }
+}
 strip = neopixel.create(DigitalPin.P1, 64, NeoPixelMode.RGB)
 strip.showColor(neopixel.colors(NeoPixelColors.Black))
 strip.setBrightness(64)
-
-
+initialize()
+initializeStars()
 basic.forever(function () {
 	
 })
